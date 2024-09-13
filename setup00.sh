9978f12f72e8043a7c0b0bfc1498da317f2ee82d
khaudio <kylehughes1@gmail.com>
2024-09-13 16:29:15 -0500

    setup00.sh is just a one liner to pull the repo

--- setup00.sh
+++ setup00.sh
@@ -2,4 +2,4 @@
 
 # Run to pull repo.  To start setup, run setup01.sh
 
-cd ~; apt update; apt -y install git && git clone https://github.com/khaudio/shairport-sync-vm.git && cd shairport-sync-vm; chmod +x ./*.sh
+apt update; apt -y install git && git clone https://github.com/khaudio/shairport-sync-vm.git && cd shairport-sync-vm; chmod +x ./*.sh
